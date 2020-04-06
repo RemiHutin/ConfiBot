e264f56982708dc3ce76c861178ebe936fd3f9ea
Rémi Hutin <remi.hutin@ens-rennes.fr>
2020-04-06 12:07:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,21 +7,16 @@ const Lobby = require('./lobby.js')
 client.on('ready', () => {
   console.log('I am ready!');
   Lobby.init(client,  {
-    "timesbomb": require('./timesbomb.js'),
-    "codenames": require('./codenames.js')
+    "timesbomb": require('./timesbomb.js')
   });
 });
 
 
-const Codenames = require('./codenames.js')
 // Create an event listener for messages
 client.on('message', message => {
   if (message.content.substring(0, 1) == '!') {
     const args = message.content.substring(1).split(' ');
     const cmd = args[0];
-    let test = new Codenames([], message.channel);
-    test.start();
-    return;
     switch(cmd) {
       case 'play':
         Lobby.play(args[1], message.author, message.channel);
@@ -37,5 +32,4 @@ client.on('message', message => {
 });
 
 // Log our bot in using the token from https://discordapp.com/developers/applications/me
-var token = JSON.parse(fs.readFileSync('auth.json')).token;
-client.login(token);
+client.login(require('./auth.json').token);
